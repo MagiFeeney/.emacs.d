d85e83e880ba677c8d6721161aa6378010022c7b
Magi Feeney <matrixfeeney@gmail.com>
2026-07-12 20:26:56 +0800

    (ido): don't read file name at point

--- completion.el
+++ completion.el
@@ -5,7 +5,6 @@
   (setq ido-enable-flex-matching t
         ido-everywhere t
         ido-create-new-buffer 'always
-        ido-use-filename-at-point 'guess
         ido-max-prospects 10
         ido-default-file-method 'selected-window
         ido-default-buffer-method 'selected-window)
